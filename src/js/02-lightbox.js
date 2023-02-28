2fbd4ed9164a641c9a7eaaffa9fa8f3313e06e8e
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-02-28 12:54:53 +0300

    второе задание

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const galleruMarkup = galleryItems.map(({preview, original, description}) =>{
+    return `
+    <li class="gallery">
+        <a class="gallery__item" href = ${original}>
+            <img 
+                class = "gallery__image"
+                src = ${preview} 
+                alt = ${description}
+            />
+        </a>
+    </li>`
+    }).join('')
+        
+    // console.log(galleruMarkup)
+    
+    galleryEl.insertAdjacentHTML('afterbegin', galleruMarkup)
+    
+    const lightbox = new SimpleLightbox('.gallery__item', { 
+        captionSelector: 'img',
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
+    })
+
